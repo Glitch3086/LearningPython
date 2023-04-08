6ea78be8bb90647e9ac6563e5884e8fccdd0ae9b
Harsh Goswami <Glitch3086@gmail.com>
2023-04-08 18:25:06 -0500

    Update 2
    Created variables, def, and dict.

--- Project 1 - Rock Paper Scissors/main.py
+++ Project 1 - Rock Paper Scissors/main.py
@@ -1,1 +1,9 @@
-# Project 1
+def get_choices():
+    player_choice = "rock"
+    computer_choice = "paper"
+    choises = {"player": player_choice, "computer": computer_choice}
+
+    return choises
+
+choises = get_choices()
+print(choises)
