6805aaf62b1eb3f52b857d8cb978114a9a8a7b0d
Harsh Goswami <Glitch3086@gmail.com>
2023-04-08 18:32:34 -0500

    Update 3
    The player is able to input their own choice and the computer is also able to make its own choices

--- Project 1 - Rock Paper Scissors/main.py
+++ Project 1 - Rock Paper Scissors/main.py
@@ -1,7 +1,11 @@
+import random
+
 def get_choices():
-    player_choice = "rock"
-    computer_choice = "paper"
+    player_choice = input("Enter a Choice (Rock, Paper, Scissor: )")
+    possible_choice = ["Rock", "Paper", "Scissor"]
+    computer_choice = random.choice(possible_choice)
     choises = {"player": player_choice, "computer": computer_choice}
+    
 
     return choises
 
