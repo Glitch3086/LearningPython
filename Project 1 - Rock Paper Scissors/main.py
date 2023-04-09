a1bf39c4082f5b6297477b4fa9a5344168bdac0b
Harsh Goswami <Glitch3086@gmail.com>
2023-04-08 19:13:45 -0500

    Update 4
    Added a check to see if the player won or lost

--- Project 1 - Rock Paper Scissors/main.py
+++ Project 1 - Rock Paper Scissors/main.py
@@ -5,9 +5,33 @@ def get_choices():
     possible_choice = ["Rock", "Paper", "Scissor"]
     computer_choice = random.choice(possible_choice)
     choises = {"player": player_choice, "computer": computer_choice}
-    
-
     return choises
 
-choises = get_choices()
-print(choises)
+def check_win(player, computer):
+    print(f"You choose: {player}, computer choose: {computer}")
+    # checks to see if there is a tie
+    if player == computer: 
+        return "It's a Tie"
+
+    # Checks for win if palyer chose rock
+    elif player == "Rock":
+        if computer == "Scissor":
+            return "Rock smaches Scissor. You Win!"
+        else:
+            return "Paper covers Rock. You Lose!"
+
+    # Checks for win if palyer chose Paper
+    elif player == "Paper":
+        if computer == "Scissor":
+            return "Scissor cuts Paper. You Lose!"
+        else:
+            return "Paper covers Rock. You Win!"
+            
+    # Checks for win if palyer chose Scissor
+    elif player == "Scissor":
+        if computer == "Rock":
+            return "Rock smaches Scissor. You Win"
+        else:
+            return "Scissor cuts Paper. You Lose!"
+
+check_win("Rock", "Paper")
